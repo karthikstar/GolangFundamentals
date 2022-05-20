fdd5ac5f9fdbcde73ec213fbafb265f62f87ee1d
karthikstar <elkarthi2000@gmail.com>
2022-05-20 16:59:43 +0800

    receiver functions done

--- demo/rcv-func/rcv-func.go
+++ demo/rcv-func/rcv-func.go
@@ -2,6 +2,35 @@ package main
 
 import "fmt"
 
-func main() {
+type Space struct {
+	occupied bool
+}
 
+type ParkingLot struct {
+	spaces []Space
+}
+
+//normal function
+func occupySpace(lot *ParkingLot, spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = true
+}
+
+// receiver function
+func (lot *ParkingLot) occupySpace(spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = true
+}
+
+func (lot *ParkingLot) vacateSpace(spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = false
+}
+
+func main() {
+	lot := ParkingLot{spaces: make([]Space, 4)}
+	fmt.Println("Initial:", lot)
+	fmt.Println(lot)
+	lot.occupySpace(1)                  // occupy space 1
+	occupySpace(&lot, 2)                // occupy space 2
+	fmt.Println("After occupied:", lot) // spaces 1 and 2 are true. 3 and 4 are false
+	lot.vacateSpace(2)
+	fmt.Println("After vacate:", lot) // space 1 is true, 2 3 4 is false
 }
--- exercise/rcv-func/rcv-func.go
+++ exercise/rcv-func/rcv-func.go
@@ -16,6 +16,29 @@ package main
 
 import "fmt"
 
-func main() {
+type Player struct {
+	Health, MaxHealth uint // u int is unsigned integer
+	Energy, MaxEnergy uint
+	Name              string
+}
 
+// receiver functions to modify player stats
+func (player *Player) modifyHealth(newHealth uint) {
+	player.Health = newHealth
+}
+
+func (player *Player) modifyEnergy(newEnergy uint) {
+	player.Energy = newEnergy
+}
+
+func main() {
+	john := Player{20, 40, 30, 50, "John"}
+	fmt.Println("Initial:")
+	fmt.Println("Health:", john.Health, "Max Health:", john.MaxHealth, "Energy:", john.Energy, "Max Energy:", john.MaxEnergy)
+	john.modifyHealth(30)
+	fmt.Println("After updating health:")
+	fmt.Println("Health:", john.Health, "Max Health:", john.MaxHealth, "Energy:", john.Energy, "Max Energy:", john.MaxEnergy)
+	john.modifyEnergy(35)
+	fmt.Println("After updating energy:")
+	fmt.Println("Health:", john.Health, "Max Health:", john.MaxHealth, "Energy:", john.Energy, "Max Energy:", john.MaxEnergy)
 }
